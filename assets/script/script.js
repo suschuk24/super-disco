1937fa39bfbd24b9a4eda37cfcabe9e52f37286f
Seth Uschuk <suschuk24@gmail.com>
2020-08-30 09:40:57 -0500

    fixed time colors to update correctly, added notes

--- assets/script/script.js
+++ assets/script/script.js
@@ -7,21 +7,27 @@ var saveBtns = []
 var todaysDate = moment().format("dddd, MMMM Do")
 $("#currentDay").text(todaysDate)
 
+var hourNow = moment().startOf('hour');
+
+
 // add time slots and save buttons to arrays
 for(var i = 7; i<= 17; i++) {
     // create vars for time moment, create block and savebutton id querys
-    var time = moment().startOf(todaysDate).add(i, 'hours')
     var block = $("#time-" + i + " textarea")
     var saveBtn = $("#time-" + i + " .saveBtn")
+    var time = moment().startOf(todaysDate).add(i, 'hours')
+
+
 
     // create ann object for later ref
     var timeBlock = {
         time: time,
-        block: block,
+        block: block
     }
     times.push(timeBlock);
     saveBtns.push(saveBtn);
 }
+
 
 reload();
 
@@ -35,10 +41,30 @@ saveBtns.forEach(function(button, index) {
           text=$("#time-" + value + " textarea" )
             .val()
             .trim()
-        toDoList[value - 9] = text;
+        toDoList[value - 7] = text;
         localStorage.setItem("toDoList", JSON.stringify(toDoList))
       })
 })
+
+//create function to change colors based on current time ****Got help from Kereem Legget on this part, couldn't get times to assign correct with moment.js using .diff
+
+var currentHour = moment().hour();
+    
+$(".time-frame").each(function(){
+    var timeBlockHour = parseInt($(this).attr("id").split("-")[1]);
+   
+    if (timeBlockHour < currentHour){
+        $(this).children('textarea').addClass("past")
+
+    }else if (timeBlockHour == currentHour){
+        $(this).removeClass("past")
+        $(this).children('textarea').addClass("present")
+
+    } else if (timeBlockHour > currentHour){ 
+        $(this).removeClass("present")
+        $(this).children('textarea').addClass("future")
+    }
+}, 600000);
 
 // load/set up local storage data
 function reload() {
@@ -55,24 +81,10 @@ function reload() {
     })
 }
 
-// auto update for live refresh
-function autoUpdate() {
-    for(var i = 0; i < times.length; i++){
-    times[i].block.removeClass("past")
-    times[i].block.removeClass("present")
-    times[i].block.removeClass("future")
 
-    // sync moment with start of time slot
-    var hourNow = moment().startOf('hour');
 
-    if(hourNow.diff(times[i].time, 'hour') === 0) {
-        times[i].block.addClass("present");
-    } else if(hourNow.diff(times[i].time, 'hour') > 0) {
-        times[i].block.addClass("past");
-    } else if(hourNow.diff(times[i].time, 'hour') < 0) {
-        times[i].block.addClass("future");
-    }}
-}
 
-autoUpdate()
-setInterval((autoUpdate()), 5000000);
+
+
+
+
